98da129d073fa77fdcc3ac062e6481573e4ca885
ehtishaam <ehti1233@gmail.com>
2026-01-02 15:55:35 +0530

    checked back

--- backend/app.py
+++ backend/app.py
@@ -312,6 +312,4 @@ def summarize_video():
 #        RUN SERVER
 # ==============================
 if __name__ == "__main__":
-    # Use the port Render gives you, or default to 10000 locally
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=True)
